8a955595d6ff0f877c3bf824f86c8a10dcef6afe
kungunil <9091923224@qq.com>
2021-08-28 19:00:22 +0800

    first commit

---
+++ src/main/java/test.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: 黄龙
+ * @Date: 2021/08/28/18:43
+ * @Description:
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("你好 小笨蛋");
+    }
+}
